89ab7b14037d7d0b0b39f10157f99e1b87c04640
Maxime Cousinou <maxime.cousinou@zemax.net>
2022-04-29 23:50:49 +0200

    fix(sass): update sass and fix svg-function

--- svg-function.js
+++ svg-function.js
@@ -1,24 +1,25 @@
-const path  = require( 'path' );
-const fs    = require( 'fs' );
-const types = require( 'sass' ).types;
+const path = require("path");
+const fs = require("fs");
+const { SassString } = require("sass");
 
-const encode_svg = require( './lib/encode-svg' );
+const encode_svg = require("./lib/encode-svg");
 
-module.exports = ( basePath = './' ) => ({
-    'svg($filename, $mapping: ())': function ( svgFileName, mapping ) {
-        const filename = path.resolve( basePath, svgFileName.getValue() );
-        let svg        = fs.readFileSync( filename, 'utf8' );
-        svg            = encode_svg( svg );
-        
-        if ( mapping instanceof types.Map ) {
-            for ( let i = 0, l = mapping.getLength(); i < l; i++ ) {
-                const key   = mapping.getKey( i ).getValue();
-                const value = mapping.getValue( i ).toString();
-                
-                svg = svg.replaceAll( `'$${key}'`, `'${encodeURIComponent( value )}'` );
-            }
-        }
-        
-        return new types.String( 'url("data:image/svg+xml,' + svg + '")' );
+module.exports = (basePath = "./") => ({
+  "svg($filename, $mapping: ())": function (svgFileName, mapping) {
+    const filename = path.resolve(basePath, svgFileName.getValue());
+    let svg = fs.readFileSync(filename, "utf8");
+    svg = encode_svg(svg);
+
+    if (mapping.getLength() > 0) {
+      for (let i = 0, l = mapping.getLength(); i < l; i++) {
+        const key = mapping.getKey(i).getValue();
+        const val = mapping.getValue(i);
+        const value = val.dartValue ? val.dartValue.toString() : val.toString();
+
+        svg = svg.replaceAll(`'$${key}'`, `'${encodeURIComponent(value)}'`);
+      }
     }
+
+    return new SassString('url("data:image/svg+xml,' + svg + '")');
+  },
 });
